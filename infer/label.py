dfdeac5ca8df9e834dec2e10ec1d58bfbb19e2d9
nknytk <nknytk.dev@gmail.com>
2020-08-23 20:11:51 +0900

    non maximum suppression

--- infer/label.py
+++ infer/label.py
@@ -6,7 +6,7 @@ import grid
 
 
 class DrawLabel:
-    def __init__(self, detection_target, img_x, img_y, n_grid_x, n_grid_y, min_proba=0.5):
+    def __init__(self, detection_target, img_x, img_y, n_grid_x, n_grid_y, min_proba=0.5, suppress_iou_threshold=0.7):
         if detection_target == 'urban_objects':
             self.labels = [
                 #('label_name', (r, g, b))
@@ -28,9 +28,12 @@ class DrawLabel:
             ]
         self.grid_retriever = grid.GridRetriever(img_x, img_y, n_grid_x, n_grid_y)
         self.min_proba = min_proba
+        self.suppress_iou_threshold = suppress_iou_threshold
 
 
     def draw_rect(self, image, prediction):
+        detected = [[] for i in range(len(self.labels))]
+
         for grid_index, vec in enumerate(prediction):
             obj_probas = softmax(vec[4:])
             obj_id = numpy.argmax(obj_probas)
@@ -39,10 +42,38 @@ class DrawLabel:
             proba = obj_probas[obj_id]
             if proba < self.min_proba:
                 continue
-            x_min, y_min, x_max, y_max = self.grid_retriever.restore_box(grid_index, *vec[:4])
 
+            bbox = self.grid_retriever.restore_box(grid_index, *vec[:4])
+            detected[obj_id].append([proba, True, bbox])
+
+        for obj_id, detections in enumerate(detected):
             _, color = self.labels[obj_id]
-            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color, thickness=2)
+            for i in range(len(detections)):
+                for j in range(len(detections)):
+                    if i == j or detections[i][1] == False or detections[j][1] == False:
+                        continue
+                    iou = self.iou(detections[i][2], detections[j][2])
+                    if iou >= self.suppress_iou_threshold:
+                        if detections[i][0] > detections[j][0]:
+                            detections[j][0] = False
+                        else:
+                            detections[i][0] = False
+
+            for proba, to_draw, bbox in detections:
+                if to_draw:
+                    cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, thickness=2)
+
+
+    def iou(self, box1, box2):
+        # x_min, y_min, x_max, y_max = box
+        if box1[2] <= box2[0] or box2[2] <= box1[0] or box1[3] <= box2[1] or box2[3] <= box1[1]:
+            return 0.0
+
+        area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+        area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+        area_shared = (min(box1[2], box2[2]) - max(box1[0], box2[0])) * (min(box1[3], box2[3]) - max(box1[1], box2[1]))
+        iou = area_shared / (area1 + area2 - area_shared)
+        return iou
 
 
 def softmax(x):
@@ -50,4 +81,3 @@ def softmax(x):
     exp_x = numpy.exp(x - c)
     sum_exp_x = numpy.sum(exp_x)
     return exp_x / sum_exp_x
-
